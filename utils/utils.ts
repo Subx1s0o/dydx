7b6a2bae5410a6faa8607a28e1599147694e1f44
Subx1s0o <nazarsabardin@gmail.com>
2025-04-26 17:09:25 +0300

    Add BigIntInterceptor for handling BigInt serialization and create utility functions

---
+++ utils/utils.ts
@@ -0,0 +1,66 @@
+import { config } from '../src/config';
+import { OrderFlags } from '@dydxprotocol/v4-client-js';
+
+export interface InstrumentMapping {
+  original: string;
+  dydx: string;
+}
+
+export function formatToDydxInstrument(instrument: string): string {
+  const baseAsset = instrument.split('/')[0];
+  return `${baseAsset}-USD`;
+}
+
+export function formatFromDydxInstrument(dydxInstrument: string): string {
+  const baseAsset = dydxInstrument.split('-')[0];
+  return `${baseAsset}/USDT`;
+}
+
+export function findMatchingInstruments(markets: any): InstrumentMapping[] {
+  const availableInstruments = Object.keys(markets);
+
+  return config.instruments
+    .map((instrument) => {
+      const dydxFormat = formatToDydxInstrument(instrument);
+
+      if (availableInstruments.includes(dydxFormat)) {
+        return {
+          original: instrument,
+          dydx: dydxFormat,
+        };
+      }
+      return null;
+    })
+    .filter((mapping): mapping is InstrumentMapping => mapping !== null);
+}
+
+export const toUnixTimestamp = (value: string | number): number => {
+  if (typeof value === 'number') return value;
+  const date = new Date(value);
+  const result = Math.floor(date.getTime() / 1000);
+
+  return result;
+};
+
+export function parseOrderFlags(orderFlags: string | number) {
+  const flagsNum = typeof orderFlags === 'string' ? +orderFlags : orderFlags;
+
+  let parsedFlags: string | undefined;
+
+  if (flagsNum === OrderFlags.SHORT_TERM) {
+    parsedFlags = 'SHORT_TERM';
+  } else if (flagsNum === OrderFlags.CONDITIONAL) {
+    parsedFlags = 'CONDITIONAL';
+  } else if (flagsNum === OrderFlags.LONG_TERM) {
+    parsedFlags = 'LONG_TERM';
+  } else {
+    parsedFlags = undefined;
+  }
+
+  const goodTilTimeInSeconds =
+    flagsNum === OrderFlags.LONG_TERM || flagsNum === OrderFlags.CONDITIONAL
+      ? 86400
+      : 0;
+
+  return { orderFlags: parsedFlags, goodTilTimeInSeconds };
+}
