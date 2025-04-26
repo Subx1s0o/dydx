86dfa664489c2f73d8f2ad589ddc04618fffa2ce
Subx1s0o <nazarsabardin@gmail.com>
2025-04-26 16:29:45 +0300

    Add Instruments module

---
+++ src/instruments/instruments.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { InstrumentsService } from './instruments.service';
+
+@Controller('instruments')
+export class InstrumentsController {
+  constructor(private readonly instrumentsService: InstrumentsService) {}
+}
